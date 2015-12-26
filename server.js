7f58a264f20164f7432bc13b1dadad476bb2648c
umaror <davidventura3@gmail.com>
2015-12-26 22:44:27 +0200

    changed port to assigned heroku path

--- server.js
+++ server.js
@@ -11,10 +11,10 @@ var server = http.createServer(function (request, response) {
 });
 
 // Listen on port 8000, IP defaults to 127.0.0.1
-server.listen("0.0.0.0",80);
+server.listen(process.env.PORT || 8000);
 
 // Put a friendly message on the terminal
-console.log("Server running at http://127.0.0.1:80/");
+console.log("Server running at http://127.0.0.1:8000/");
 
 var client = new Twitter({
   consumer_key: 'yQLXZwiw7PbyNSGngurCm0qEl',
